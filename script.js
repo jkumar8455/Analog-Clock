066edd886a03eda79e5ab3eaeed783b7e2dba779
vader <jkumar8455@gmail.com>
2023-02-18 21:57:31 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let hourEle=document.getElementById("hour");
-let miunteEle=document.getElementById("minute");
+let minuteEle=document.getElementById("minute");
 let secondEle=document.getElementById("second");
 
 setInterval(() => {
@@ -9,6 +9,9 @@ setInterval(() => {
     let minute=date.getMinutes();
     console.log(hour+" "+minute+" "+second);
     hourEle.style.transform=`rotate(${30*hour+minute/2}deg)`;
-    miunteEle.style.transform=`rotate(${6*minute+second/10}deg)`;
+    minuteEle.style.transform=`rotate(${6*minute+second/10}deg)`;
     secondEle.style.transform=`rotate(${6*second}deg)`;
+    hourEle.style.display="block";
+    minuteEle.style.display="block";
+    secondEle.style.display="block";
 }, 1000);
